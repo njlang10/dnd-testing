018d5a88a1ad83a6bde3cc49b17887f5c8be2625
Nick <njlang10@gmail.com>
2022-10-24 20:48:53 +0000

    Add rows rendering

--- src/index.tsx
+++ src/index.tsx
@@ -214,13 +214,66 @@ function BlockContainerView({
   );
 }
 
+/**
+ * Manage all rows
+ */
+const ROW_ACCEPTS_DROPS_FROM: DraggableTypes[] = ["BLOCK", "CONTAINER", "ROW"];
+function RowContainerView({
+  rows,
+  onDrop,
+}: {
+  rows: RowContainer[];
+  onDrop: OnDropFunc;
+}): JSX.Element {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        minHeight: "50px",
+      }}
+    >
+      {rows.map((rowItem, rowIdx) => {
+        return (
+          <React.Fragment key={rowIdx}>
+            <DropContainer
+              key={`pre_row_drop_${rowIdx}`}
+              dropLocation="PRE_ROW"
+              acceptedTypes={ROW_ACCEPTS_DROPS_FROM}
+              coordinates={{ rowIdx }}
+              orientation={"VERTICAL"}
+              onDrop={onDrop}
+            />
+            <BlockContainersView
+              rowContainer={rowItem}
+              coordinates={{ rowIdx }}
+              onDrop={onDrop}
+            />
+          </React.Fragment>
+        );
+      })}
+      <DropContainer
+        key={`post_row_drop_${rows.length}`}
+        dropLocation="PRE_ROW"
+        acceptedTypes={ROW_ACCEPTS_DROPS_FROM}
+        coordinates={{ rowIdx: rows.length }}
+        orientation={"VERTICAL"}
+        onDrop={onDrop}
+      />
+    </div>
+  );
+}
+
+/**
+ * Manage layout of containers within a row
+ */
 const CONTAINER_LAYOUT: ContainerOrientation = "HORIZONTAL";
 const CONTAINER_ACCEPTS_DROPS_FROM: DraggableTypes[] = [
   "BLOCK",
   "CONTAINER",
   "ROW",
 ];
-
 function BlockContainersView({
   rowContainer,
   coordinates,
